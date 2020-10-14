959dc11463123575a56c7e2ab10878f4428d7fa8
trevorbautista <trevorb@hueychich.org>
2020-10-14 16:21:21 -0700

    notes (with basic session)

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session # Be able to store sessions server-side
 
 # Create new Flask web application
 app = Flask(__name__)
+# Use the FileSystemSessionInterface session type
+app.config["SESSION_TYPE"] = "filesystem"
+# Don't use a permanent session
+app.config["SESSION_PERMANENT"] = False
+# Create the Session object, passing it the application
+Session(app)
 
-# List of user's notes
-# NOTE: this is global accross the whole app
-notes = []
+
 
 # Webroot
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if session.get("notes") is None:
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        notes.append(note)
+        session["notes"].append(note)
 
-    return render_template("index.html", notes=notes)
+    return render_template("index.html", notes=session["notes"])

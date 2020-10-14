861904f1a264a4aadd527a551464005a0c24a870
trevorbautista <trevorb@hueychich.org>
2020-10-14 15:48:24 -0700

    notes (no session)

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from flask import Flask, render_template, request
 # Create new Flask web application
 app = Flask(__name__)
 
-# Webroot
-@app.route("/")
-def index():
-    return render_template("index.html")
+# List of user's notes
+# NOTE: this is global accross the whole app
+notes = []
 
-@app.route("/hello", methods=["GET", "POST"])
-def hello():
-    if request.method == "GET":
-        return "Please submit the form instead."
-    else:
-        name = request.form.get("name")
-        return render_template("hello.html", name=name)
+# Webroot
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        note = request.form.get("note")
+        notes.append(note)
+
+    return render_template("index.html", notes=notes)

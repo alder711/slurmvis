485c3f01aa95aa3dbafd7728a7b5e2c67ce03787
trevorbautista <trevorb@hueychich.org>
2020-10-14 13:49:00 -0700

    jinja2 loops

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ app = Flask(__name__)
 # Webroot
 @app.route("/")
 def index():
-    now = datetime.datetime.now()
-    new_year = now.month == 1 and now.day == 1
-    return render_template("index.html", new_year=new_year)
+    names = ["Alice", "Bob", "Charlie"]
+    return render_template("index.html", names=names)
